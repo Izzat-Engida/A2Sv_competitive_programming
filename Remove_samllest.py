4bf4e281d18c90c716bacb463ef1fbca178b10c3
Izzat <124386339+Izzat-Engida@users.noreply.github.com>
2024-03-29 17:27:05 +0300

    Update Remove_samllest.py

--- Remove_samllest.py
+++ Remove_samllest.py
@@ -1,21 +1,13 @@
-t=int(input())
-while t>0:
-    n=int(input())
-    arr = list(map(int, input().split()))
-    i=0
-    j=n-1
-    while j!=i:
-        if abs(arr[i]-arr[j])<=1:
-            if arr[i]>arr[j]:
-                del arr[j]
-            elif arr[i]<arr[j]:
-                del arr[i]
-            else:
-                del arr[j]       
-        j-=1
-    if len(arr)==1:
-        print("YES")
-    else:
-        print("NO")        
-
-    t-=1
+    t=int(input())
+    while t>0:
+        n=int(input())
+        arr = list(map(int, input().split()))
+        arr.sort()
+        for i in range(n-1):
+            if abs(arr[i]-arr[i+1])>1:
+                print("NO")
+                break
+        else:
+            print("Yes")
+     
+        t-=1
