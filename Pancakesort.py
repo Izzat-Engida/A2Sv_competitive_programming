4ec1805c0b34853ddf6da62f3c9949690c42b209
Izzat <124386339+Izzat-Engida@users.noreply.github.com>
2024-04-12 19:34:35 +0300

    Update Pancakesort.py

--- Pancakesort.py
+++ Pancakesort.py
@@ -1,22 +1,9 @@
 class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
-        def check(arr):
-            for i in range(1,len(arr)):
-                if arr[i-1]>arr[i]:
-                    return False
-            return True
-        j=len(arr)
-        ans=[]
-        while not check(arr):
-            maxi=0
-            maxv=arr[0]
-            for i in range(1,j):
-                if maxv<arr[i]:
-                    maxv=arr[i]
-                    maxi=i
-            arr[:maxi + 1]=reversed(arr[:maxi+1])
-            ans.append(maxi+1)
-            arr[:j]=reversed(arr[:j])
-            ans.append(j)
-            j-=1
-        return ans                        
+        ans = []
+        for size in reversed(range(1, len(arr) + 1)):
+            max_idx = arr.index(max(arr[:size]))
+            arr[:max_idx + 1] = reversed(arr[:max_idx + 1])
+            arr[:size] = reversed(arr[:size])
+            ans.extend([max_idx + 1, size])
+        return ans
