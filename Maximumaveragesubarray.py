a669e19ac9a0a933e1fe88328b76bd7f94e43e5a
Izzat <124386339+Izzat-Engida@users.noreply.github.com>
2024-04-12 11:58:54 +0300

    Update Maximumaveragesubarray.py

--- Maximumaveragesubarray.py
+++ Maximumaveragesubarray.py
@@ -1,19 +1,26 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        ma=0
-        total=0
-        for i in range(k):
-            total+=nums[i]
-        ma=total/k
-        r=k
-        l=0
-        while r<len(nums):
-            total-=nums[l]
-            total+=nums[r]
-            r+=1
-            l+=1
-            ma=max(ma,total/k)
-        return ma        
+        ma=float('-inf')
+        i=0
+        j=k
+        su=0
+        while i<j and i<len(nums):
+            su+=nums[i]
+            i+=1
+        ma=max(ma,su/k)
+         
+        while j<len(nums) and i<len(nums):
+            
+            su-=nums[i-k]
+            su+=nums[j]
+            i+=1
+            j+=1
+           
+            ma=max(ma,su/k)
+        return ma    
+            
+
+
 
 
         
