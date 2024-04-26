8d098be0b48c22f3b90e2875355f53674781b7b5
Izzat <124386339+Izzat-Engida@users.noreply.github.com>
2024-04-26 17:34:16 +0300

    Update Largest_number.py

--- Largest_number.py
+++ Largest_number.py
@@ -1,8 +1,7 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        if nums==[999999991,9]:
-            return "9999999991"
-        nums=sorted(nums,key=lambda x:str(x)*3,reverse=True )
+    
+        nums=sorted(nums,key=lambda x:str(x)*9,reverse=True )
         ans=''.join(map(str,nums))
         return '0' if ans[0]=='0' else ans
        
