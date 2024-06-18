b2d3b8f06b3804a6dcf8b7e4489a29efaa8ee986
Izzat <124386339+Izzat-Engida@users.noreply.github.com>
2024-06-18 21:51:56 +0300

    Create rightSideView.py

---
+++ rightSideView.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+        order=[]
+        queue=[]
+        queue.append(root)
+        while queue:
+            size=len(queue)
+            temp=[]
+            for i in range(size):
+                node=queue[0]
+                queue=queue[1:]
+                temp.append(node.val)
+                if node.left is not None:
+                    queue.append(node.left)
+                if node.right is not None:
+                    queue.append(node.right)
+            order.append(temp[-1])
+        return order         
+      
+            
+        
